c3bd669bfde45cbba67717f947c42ce68aa75f7f
AdrienHq <adrienhecq@gmail.com>
2024-04-16 17:23:26 +0200

    feat: new redirection

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('task.index');
-});
+Route::get('/', [\App\Http\Controllers\TaskController::class, 'index']);
+Route::get('/create', [\App\Http\Controllers\TaskController::class, 'create']);
+Route::post('/create', [\App\Http\Controllers\TaskController::class, 'store']);
+
+//Route::get('/task/create', function () {
+//    return view('task.create');
+//});
