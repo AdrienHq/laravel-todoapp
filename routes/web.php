91febf13ad7941a07ac0626190aa0365661780d9
AdrienHq <adrienhecq@gmail.com>
2024-04-17 17:03:47 +0200

    feat: new edit route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,5 @@ Route::get('/', [TaskController::class, 'index'])->name('index');
 Route::get('/create', [TaskController::class, 'create'])->name('task.create');
 Route::post('/create', [TaskController::class, 'store']);
 
-//Route::get('/task/create', function () {
-//    return view('task.create');
-//});
+Route::get('/{task}/edit', [TaskController::class, 'edit'])->name('task.edit');
+Route::post('/{task}/edit', [TaskController::class, 'update']);
