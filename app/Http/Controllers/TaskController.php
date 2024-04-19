712cb8b7993754b074aff9457e3fb65a1bd76e24
AdrienHq <adrienhecq@gmail.com>
2024-04-19 16:24:00 +0200

    feat: all route

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,19 +5,25 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CreateTaskRequest;
 use App\Models\Task;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        $tasks = Task::paginate(8);
-
         $todayTasks = Task::whereDate('created_at', today())->get();
 
 
         return view('task.index',[
-            'tasks' => $tasks,
+            'tasks' => DB::table('tasks')->simplePaginate(7),
             'todayTasks' => $todayTasks
+        ]);
+    }
+
+    public function all()
+    {
+        return view('task.all',[
+            'tasks' => DB::table('tasks')->simplePaginate(7),
         ]);
     }
 
@@ -34,7 +40,7 @@ class TaskController extends Controller
     {
         $task = Task::create($request->validated());
 
-        return redirect()->route('index')->with('success', "Task created");
+        return redirect()->route('index')->with('success', "Task created successfully");
     }
 
     public function edit(Task $task)
@@ -48,6 +54,12 @@ class TaskController extends Controller
     {
         $task->update($request->validated());
 
-        return redirect()->route('index')->with('success', "Task updated");
+        return redirect()->route('index')->with('success', "Task updated successfully");
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect()->route('index')->with('success', 'Task deleted successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,8 @@ Route::post('/create', [TaskController::class, 'store']);
 
 Route::get('/{task}/edit', [TaskController::class, 'edit'])->name('task.edit');
 Route::post('/{task}/edit', [TaskController::class, 'update']);
+
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+
+Route::get('/all', [TaskController::class, 'all'])->name('all');
+
